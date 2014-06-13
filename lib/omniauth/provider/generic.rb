7710c820e304d0b772a3320507dc452d816aa4b1
German DZ <germ@ndz.com.ar>
2014-06-13 18:33:56 +0200

    remove "extra info" from omniauth data
    too large for a cookie session

--- lib/omniauth/provider/generic.rb
+++ lib/omniauth/provider/generic.rb
@@ -5,12 +5,13 @@ module OmniAuth
       def self.init(provider_name)
         klass = self
         OmniAuth::MultiProvider::CallbacksController.add_callback(provider_name) do
-          resource = klass.find_from_oauth(provider_name, request.env['omniauth.auth'], send(OmniAuth::MultiProvider::current_resource))
+          auth = request.env['omniauth.auth']
+          resource = klass.find_from_oauth(provider_name, auth, send(OmniAuth::MultiProvider::current_resource))
           if resource.persisted?
             sign_in_and_redirect resource, event: :authentication #this will throw if resource is not activated
             set_flash_message(:notice, :success, kind: provider_name.to_s.camelize) if is_navigational_format?
           else
-            session["devise.#{provider_name}_data"] = auth
+            session["devise.#{provider_name}_data"] = auth.except('extra')
             redirect_to send("new_#{OmniAuth::MultiProvider::resource_mapping}_registration_url")
           end
         end
