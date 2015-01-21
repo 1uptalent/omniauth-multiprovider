b5a47557daa81de672e13381480c42a6327c8b1a
Abel Muiño <amuino@1uptalent.com>
2015-01-21 17:01:06 +0100

    FIX: the provider does not have access to protected controller methods

--- lib/omniauth/provider/generic.rb
+++ lib/omniauth/provider/generic.rb
@@ -8,7 +8,7 @@ module OmniAuth
         resource = resource_class.from_oauth auth, signed_in_resource
         if resource.persisted?
           sign_in_and_redirect resource, event: :authentication #this will throw if resource is not activated
-          set_flash_message(:notice, :success, kind: provider_name.to_s.camelize) if is_navigational_format?
+          @controller.send(:set_flash_message, :notice, :success, kind: provider_name.to_s.camelize) if is_navigational_format?
         else
           session["devise.#{provider_name}_data"] = auth.except('extra')
           redirect_to send("new_#{OmniAuth::MultiProvider::resource_mapping}_registration_url")
