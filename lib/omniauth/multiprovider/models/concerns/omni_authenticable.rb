f39d2152d7e23e2914227a745a2699337423b3b4
Abel Muiño <amuino@1uptalent.com>
2015-01-22 14:23:10 +0100

    Extension points for the models

--- lib/omniauth/multiprovider/models/concerns/omni_authenticable.rb
+++ lib/omniauth/multiprovider/models/concerns/omni_authenticable.rb
@@ -26,28 +26,54 @@ module OmniAuth
               return signed_in_resource
             end
             unless resource
-              if auth.info[:email].blank?
-                auth.info[:email] = mock_email(provider_name, auth.uid)
+              attributes = oauth_to_attributes auth
+              if signed_in_resource
+                signed_in_resource.update_from_oauth attributes
+                resource = signed_in_resource
+              else
+                resource = create_from_oauth attributes
               end
-              email = auth.info[:email]
-
-              raise OmniAuth::MultiProvider::EmailTakenError if find_by(email: email)
-
-              resource = signed_in_resource || new(email: email, password: Devise.friendly_token[0,20])
-
               _oamp.authentication_klass.from(auth, resource)
-
             end
             resource
+          end
+
+          # Can be customized in each model
+          def oauth_to_attributes(oauth_data)
+            {
+              email: auth.info[:email] || mock_email(provider_name, auth.uid),
+              password: Devise.friendly_token[0,20]
+            }
+          end
+
+          # Can be customized in each model
+          def create_from_oauth(attributes, signed_in_resource = nil)
+            raise OmniAuth::MultiProvider::EmailTakenError if exists?(email: attributes[:email])
+            create!(attributes)
           end
         end
 
         # Handler for the case when there is already an Authentication
+        # Can be customized in each model
         def oauth_already_bound(other, oauth_data)
-          return self if self == other # do nothing if its own authentication
+          return self if self == other # do nothing if its own authentication, potentially update
           # raise error if the oauth data is bound to another resource
-          raise MultiProvider::AlreadyBoundError.new signed_in_resource, new_resource
+          raise MultiProvider::AlreadyBoundError.new self, other
         end
+
+        # Handler for the case when a signed_in_resource exists and is being authenticated
+        # Can be customized in each model
+        def update_from_oauth(new_attrs)
+          unless self.email.blank? || new_attrs[:email] == self.email
+            # refusing to change existing email
+            new_attrs.delete(:email)
+            # but check if someone else is using it anyway
+            raise OmniAuth::MultiProvider::EmailTakenError if exists?(email: new_attrs[:email])
+          end
+          self.update!(new_attrs)
+        end
+
+
 
         has_many _oamp.authentication_relationship_name,
           dependent: :destroy,
